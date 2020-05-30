2194e6cca4504085e5ffedfccbfd3b15a6032e46
Dawid Komisarczuk <komidawi@gmail.com>
2020-05-30 13:20:36 +0200

    [PCC-8] Feat: Add Share Intent

--- app/src/main/java/com/github/komidawi/pizzacostcalculator/AboutFragment.kt
+++ app/src/main/java/com/github/komidawi/pizzacostcalculator/AboutFragment.kt
@@ -1,10 +1,10 @@
 package com.github.komidawi.pizzacostcalculator
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.core.app.ShareCompat
 
 class AboutFragment : Fragment() {
 
@@ -12,6 +12,35 @@ class AboutFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        setHasOptionsMenu(true)
         return inflater.inflate(R.layout.fragment_about, container, false)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+
+        inflater.inflate(R.menu.about_menu, menu)
+        if (null == getShareIntent().resolveActivity(requireActivity().packageManager)) {
+            menu.findItem(R.id.share).isVisible = false
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.share -> shareSuccess()
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun shareSuccess() {
+        startActivity(getShareIntent())
+    }
+
+    private fun getShareIntent(): Intent {
+        return ShareCompat.IntentBuilder.from(requireActivity())
+            .setText("I'm usin' wonderful Pizza App!")
+            .setType("text/plain")
+            .intent
     }
 }

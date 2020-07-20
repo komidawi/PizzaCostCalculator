e0fa5ed527f95407c52ced2fc66739f727365da8
Dawid Komisarczuk <komidawi@gmail.com>
2020-07-20 21:59:46 +0200

    [PCC-21] Feat: Create Pizza Entity

---
+++ app/src/main/java/com/github/komidawi/pizzacostcalculator/data/PizzaEntity.kt
@@ -0,0 +1,17 @@
+package com.github.komidawi.pizzacostcalculator.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "pizza_table")
+data class PizzaEntity(
+
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0L,
+
+    val name: String,
+
+    val size: Float,
+
+    val price: Float
+)

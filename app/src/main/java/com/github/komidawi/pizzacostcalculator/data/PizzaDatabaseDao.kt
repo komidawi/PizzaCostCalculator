d33f3c451e7773340f8fb61af0e9fa48d540bf89
Dawid Komisarczuk <komidawi@gmail.com>
2020-07-21 16:28:17 +0200

    [PCC-21] Feat: Create database Dao

---
+++ app/src/main/java/com/github/komidawi/pizzacostcalculator/data/PizzaDatabaseDao.kt
@@ -0,0 +1,20 @@
+package com.github.komidawi.pizzacostcalculator.data
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface PizzaDatabaseDao {
+
+    @Insert
+    fun insert(pizzaEntity: PizzaEntity)
+
+    @Query("SELECT * FROM pizza_table ORDER BY id DESC")
+    fun getAll(): LiveData<List<PizzaEntity>>
+
+    @Update
+    fun update(pizzaEntity: PizzaEntity)
+
+    @Query("DELETE FROM pizza_table WHERE id = :id")
+    fun delete(id: Long)
+}

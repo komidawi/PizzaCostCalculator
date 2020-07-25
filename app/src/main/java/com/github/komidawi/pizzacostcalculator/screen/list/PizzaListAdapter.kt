0ff76676a33b49ace5cbb4344e7c77127cbdca36
Dawid Komisarczuk <komidawi@gmail.com>
2020-07-25 12:27:19 +0200

    [PCC-25] Refactor: Encapsulate ViewHolder-creating related code

--- app/src/main/java/com/github/komidawi/pizzacostcalculator/screen/list/PizzaListAdapter.kt
+++ app/src/main/java/com/github/komidawi/pizzacostcalculator/screen/list/PizzaListAdapter.kt
@@ -17,25 +17,35 @@ class PizzaListAdapter : RecyclerView.Adapter<PizzaListAdapter.PizzaItemViewHold
         }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PizzaItemViewHolder {
-        val inflater = LayoutInflater.from(parent.context)
-        val textView = inflater.inflate(R.layout.list_item_pizza, parent, false)
-        return PizzaItemViewHolder(textView)
+        return PizzaItemViewHolder.from(parent)
     }
 
     override fun getItemCount() = data.size
 
     override fun onBindViewHolder(holder: PizzaItemViewHolder, position: Int) {
         val pizza = data[position]
-        holder.apply {
+        holder.bind(pizza)
+    }
+
+    class PizzaItemViewHolder private constructor(itemView: View) :
+        RecyclerView.ViewHolder(itemView) {
+
+        private val name: TextView = itemView.findViewById(R.id.pizza_name_display)
+        private val size: TextView = itemView.findViewById(R.id.pizza_size_display)
+        private val price: TextView = itemView.findViewById(R.id.pizza_price_display)
+
+        fun bind(pizza: PizzaEntity) {
             name.text = pizza.name
             size.text = pizza.size.toString()
             price.text = pizza.price.toString()
         }
-    }
 
-    class PizzaItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val name: TextView = itemView.findViewById(R.id.pizza_name_display)
-        val size: TextView = itemView.findViewById(R.id.pizza_size_display)
-        val price: TextView = itemView.findViewById(R.id.pizza_price_display)
+        companion object {
+            fun from(parent: ViewGroup): PizzaItemViewHolder {
+                val inflater = LayoutInflater.from(parent.context)
+                val textView = inflater.inflate(R.layout.list_item_pizza, parent, false)
+                return PizzaItemViewHolder(textView)
+            }
+        }
     }
 }

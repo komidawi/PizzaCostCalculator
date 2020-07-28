3a7458c89068e55d64a054e16921b0ab8e921322
Dawid Komisarczuk <komidawi@gmail.com>
2020-07-28 19:39:37 +0200

    [PCC-25] Feat: Add click listener to RecyclerView

--- app/src/main/java/com/github/komidawi/pizzacostcalculator/screen/list/PizzaListAdapter.kt
+++ app/src/main/java/com/github/komidawi/pizzacostcalculator/screen/list/PizzaListAdapter.kt
@@ -8,7 +8,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.github.komidawi.pizzacostcalculator.data.db.PizzaEntity
 import com.github.komidawi.pizzacostcalculator.databinding.ListItemPizzaBinding
 
-class PizzaListAdapter :
+class PizzaListAdapter(private val clickListener: PizzaItemListener) :
     ListAdapter<PizzaEntity, PizzaListAdapter.PizzaItemViewHolder>(PizzaDiffCallback()) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PizzaItemViewHolder {
@@ -16,16 +16,17 @@ class PizzaListAdapter :
     }
 
     override fun onBindViewHolder(holder: PizzaItemViewHolder, position: Int) {
-        val pizza = getItem(position)
-        holder.bind(pizza)
+        val pizza = getItem(position)!!
+        holder.bind(clickListener, pizza)
     }
 
 
     class PizzaItemViewHolder private constructor(private val binding: ListItemPizzaBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
-        fun bind(pizza: PizzaEntity) {
+        fun bind(clickListener: PizzaItemListener, pizza: PizzaEntity) {
             binding.pizzaEntity = pizza
+            binding.clickListener = clickListener
             binding.executePendingBindings()
         }
 
@@ -45,4 +46,8 @@ class PizzaDiffCallback : DiffUtil.ItemCallback<PizzaEntity>() {
 
     override fun areContentsTheSame(oldItem: PizzaEntity, newItem: PizzaEntity) =
         oldItem == newItem
+}
+
+class PizzaItemListener(val clickListener: (pizzaId: Long) -> Unit) {
+    fun onClick(pizza: PizzaEntity) = clickListener(pizza.id)
 }
--- app/src/main/java/com/github/komidawi/pizzacostcalculator/screen/list/PizzaListFragment.kt
+++ app/src/main/java/com/github/komidawi/pizzacostcalculator/screen/list/PizzaListFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.navigation.findNavController
@@ -28,7 +29,9 @@ class PizzaListFragment : Fragment() {
         viewModel = ViewModelFactory(databaseDao).create(PizzaListFragmentViewModel::class.java)
         binding.pizzaListFragmentViewModel = viewModel
 
-        val adapter = PizzaListAdapter()
+        val adapter = PizzaListAdapter(PizzaItemListener { pizzaId ->
+            Toast.makeText(context, "$pizzaId", Toast.LENGTH_SHORT).show()
+        })
         binding.recyclerView.adapter = adapter
 
         viewModel.pizzaList.observe(viewLifecycleOwner, Observer {

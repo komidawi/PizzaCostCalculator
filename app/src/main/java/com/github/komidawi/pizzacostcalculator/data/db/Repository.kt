5de819743173123fcaade8edd6b3be0e1bb7debd
Dawid Komisarczuk <komidawi@gmail.com>
2020-08-08 11:22:31 +0200

    [PCC-33] Refactor: Remove unused code

--- app/src/main/java/com/github/komidawi/pizzacostcalculator/data/db/Repository.kt
+++
@@ -1,7 +0,0 @@
-package com.github.komidawi.pizzacostcalculator.data.db
-
-import com.github.komidawi.pizzacostcalculator.data.model.PizzaModel
-
-interface Repository {
-    fun add(pizza: PizzaModel)
-}
--- app/src/main/java/com/github/komidawi/pizzacostcalculator/data/model/PizzaModel.kt
+++
@@ -1,9 +0,0 @@
-package com.github.komidawi.pizzacostcalculator.data.model
-
-import java.io.Serializable
-
-data class PizzaModel(
-    val name: String,
-    val size: Float,
-    val price: Float
-) : Serializable

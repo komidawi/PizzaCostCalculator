76ba45fe03042cf8ef8393de781e6bd9ebb3b01c
Dawid Komisarczuk <komidawi@gmail.com>
2020-08-08 20:02:32 +0200

    [PCC-28] Test: Add input tests for adding Pizza

--- app/src/test/java/com/github/komidawi/pizzacostcalculator/screen/add/AddPizzaFragmentViewModelTest.kt
+++ app/src/test/java/com/github/komidawi/pizzacostcalculator/screen/add/AddPizzaFragmentViewModelTest.kt
@@ -1,14 +1,14 @@
 package com.github.komidawi.pizzacostcalculator.screen.add
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
 import com.github.komidawi.pizzacostcalculator.data.db.PizzaEntity
 import com.github.komidawi.pizzacostcalculator.screen.factory.ViewModelFactory
-import org.junit.Assert.assertEquals
+import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import java.math.BigDecimal
-
 
 class AddPizzaFragmentViewModelTest {
 
@@ -22,27 +22,61 @@ class AddPizzaFragmentViewModelTest {
     @Before
     fun initializeViewModel() {
         viewModel =
-            ViewModelFactory(TestDatabaseDao()).create(AddPizzaFragmentViewModel::class.java)
+            ViewModelFactory(DummyDatabaseDao()).create(AddPizzaFragmentViewModel::class.java)
     }
 
     @Test
-    fun createPizza() {
+    fun createPizzaWithValidInput_returnsPizza() {
         // given
-        val testName = "TestPizzaName"
-        val testSize = "42"
-        val testValue = "30"
         val expectedPizza = PizzaEntity(0L, testName, BigDecimal(testSize), BigDecimal(testValue))
 
         // when
-        viewModel.apply {
-            name.value = testName
-            size.value = testSize
-            price.value = testValue
-        }
-
+        setValidTestPizzaData(viewModel)
         val createdPizza = viewModel.createPizza()
 
         // then
         assertEquals(expectedPizza, createdPizza)
+    }
+
+    @Test
+    fun createPizzaWithNullValue_returnsNull() {
+        // when
+        val createdPizza = viewModel.createPizza()
+
+        // expected
+        assertNull(createdPizza)
+    }
+
+
+    @Test
+    fun handleAddPizzaWithNullValue_triggersToastEvent() {
+        // given
+        val observer = Observer<Boolean> { }
+        viewModel.displayEmptyFieldsToast.observeForever(observer)
+
+        try {
+            // when
+            viewModel.handleAddPizza()
+
+            // then
+            assertTrue(viewModel.displayEmptyFieldsToast.value!!)
+
+        } finally {
+            viewModel.displayEmptyFieldsToast.removeObserver(observer)
+        }
+    }
+
+    companion object {
+        private const val testName = "TestPizzaName"
+        private const val testSize = "42"
+        private const val testValue = "30"
+
+        fun setValidTestPizzaData(viewModel: AddPizzaFragmentViewModel) {
+            viewModel.apply {
+                name.value = testName
+                size.value = testSize
+                price.value = testValue
+            }
+        }
     }
 }

82d01a93ca1ef396082d8688db01ddb35411ad8f
Dawid Komisarczuk <komidawi@gmail.com>
2020-08-12 17:41:33 +0200

    [PCC-28] Test: Add TestCoroutineDispatcher

--- app/src/test/java/com/github/komidawi/pizzacostcalculator/screen/add/AddPizzaFragmentViewModelTest.kt
+++ app/src/test/java/com/github/komidawi/pizzacostcalculator/screen/add/AddPizzaFragmentViewModelTest.kt
@@ -1,15 +1,24 @@
 package com.github.komidawi.pizzacostcalculator.screen.add
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.github.komidawi.pizzacostcalculator.data.db.PizzaDatabaseDao
 import com.github.komidawi.pizzacostcalculator.data.db.PizzaEntity
 import com.github.komidawi.pizzacostcalculator.getOrAwaitValue
 import com.github.komidawi.pizzacostcalculator.screen.factory.ViewModelFactory
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runBlockingTest
+import kotlinx.coroutines.test.setMain
+import org.junit.After
 import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import java.math.BigDecimal
 
+@ExperimentalCoroutinesApi
 class AddPizzaFragmentViewModelTest {
 
     // This rule is mandatory to use ViewModel with LiveData
@@ -17,12 +26,23 @@ class AddPizzaFragmentViewModelTest {
     @get:Rule
     var instantExecutorRule = InstantTaskExecutorRule()
 
+    private val testDispatcher = TestCoroutineDispatcher()
+
     private lateinit var viewModel: AddPizzaFragmentViewModel
 
+    private lateinit var databaseDao: PizzaDatabaseDao
+
     @Before
-    fun initializeViewModel() {
-        viewModel =
-            ViewModelFactory(DummyDatabaseDao()).create(AddPizzaFragmentViewModel::class.java)
+    fun initialize() {
+        Dispatchers.setMain(testDispatcher)
+        databaseDao = FakeDatabaseDao()
+        viewModel = ViewModelFactory(databaseDao).create(AddPizzaFragmentViewModel::class.java)
+    }
+
+    @After
+    fun tearDownDispatcher() {
+        Dispatchers.resetMain()
+        testDispatcher.cleanupTestCoroutines()
     }
 
     @Test
@@ -55,6 +75,21 @@ class AddPizzaFragmentViewModelTest {
 
         // then
         assertTrue(viewModel.displayEmptyFieldsToast.getOrAwaitValue())
+    }
+
+    @Test
+    fun handleAddPizzaWithValidValue_insertsPizzaToDatabase() = runBlockingTest {
+        setValidTestPizzaData(viewModel)
+        val createdPizza = viewModel.createPizza()!!
+        viewModel.handleAddPizza()
+
+        // then
+        val receivedPizza = databaseDao.getById(createdPizza.id)
+        val all = databaseDao.getAll()
+
+        // TODO does not work
+        assertEquals(1, all.value?.size)
+        assertEquals(createdPizza, receivedPizza)
     }
 
     companion object {

15d8e122332e971b2433816ed755f95d2e37d7c5
Dawid Komisarczuk <komidawi@gmail.com>
2021-09-30 23:51:45 +0200

    [PCC-114] Test: Provide sample dummy file for verification

---
+++ app/src/test/java/com/github/komidawi/pizzacostcalculator/screen/DummyFileTest.kt
@@ -0,0 +1,12 @@
+package com.github.komidawi.pizzacostcalculator.screen
+
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class DummyFileTest {
+
+    @Test
+    fun testXYZ() {
+        assertTrue(DummyFile().test())
+    }
+}
